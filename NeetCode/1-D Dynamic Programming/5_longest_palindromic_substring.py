ff27e5e9c5aacc8b8c5a13f4efa004531965f788
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-18 16:30:27 -0700

    Update DP approach

--- NeetCode/1-D Dynamic Programming/5_longest_palindromic_substring.py
+++ NeetCode/1-D Dynamic Programming/5_longest_palindromic_substring.py
@@ -23,19 +23,17 @@ def longest_palindrom(s: str):
     #     return n
     # dp = [[0] * n] * n
     # for i in range(n):
-    #     dp[i][i] = True
-    # max_len = 1
-    # for i in range(n - 1, -1, -1):
-    #     for dist in range(1, n - i):
-    #         j = i + dist
-    #         if s[i] == s[j]:
-    #             if dist == 1:
-    #                 dp[i][j] = True
-    #             else:
-    #                 dp[i][j] = dp[i + 1][j - 1]
-    #         if dp[i][j] == True and (j - i) + 1 > max_len:
-    #             max_len = (j - i) + 1
-    # return max_len
+    #     dp[i][i] = 1
+    # for l in range(2, n - 1):
+    #     for i in range(n - l - 1):
+    #         j = i + l - 1
+    #         if l == 2 and s[i] == s[j]:
+    #               dp[i][j] = 2
+    #         elif s[i] == s[j]:
+    #               dp[i][j] = dp[i + 1][j - 1] + 2
+    #         else:
+    #               dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+    # return dp[0][n - 1]
 
     # Expand from Center Approach:
     # Space Complexity: O(1)

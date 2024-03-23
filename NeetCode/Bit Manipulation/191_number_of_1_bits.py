8130e1c99427e2c52daf548651c8356f16a7c881
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-23 00:49:46 -0700

    Add Neetcode Bit Manipulation: Counting Bits

--- NeetCode/Bit Manipulation/191_number_of_1_bits.py
+++ NeetCode/Bit Manipulation/191_number_of_1_bits.py
@@ -4,7 +4,7 @@
 Write a function that takes the binary representation of a positive integer
 and returns the number of set bits it has (also known as the Hamming weight).
 
-Example 1:
+Example:
     Input: n = 11
     Output: 3
 """
---
+++ NeetCode/Bit Manipulation/338_counting_bits.py
@@ -0,0 +1,28 @@
+"""
+338. Counting Bits
+
+Given an integer n,
+return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ans[i] is the number of 1's in the binary representation of i.
+
+Example:
+    Input: n = 2
+    Output: [0,1,1]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+"""
+
+from typing import List
+
+
+def count_bits(n: int) -> List[int]:
+    # Time Complexity: O(n)
+    dp = [0] * (n + 1)
+    offset = 1
+    for i in range(1, n + 1):
+        if offset * 2 == i:
+            offset = i
+        dp[i] = 1 + dp[i - offset]
+    return dp

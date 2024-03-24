da67a1b0bb0512795d7620651ef2fc9f96fc11d3
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-24 14:00:49 -0700

    Add Neetcode Trees: Kth Smallest Element in a BST

---
+++ NeetCode/Trees/230_kth_smallest_element_in_a_bst.py
@@ -0,0 +1,34 @@
+"""
+230. Kth Smallest Element in a BST
+
+Given the root of a binary search tree,and an integer k,
+return the kth smallest value (1-indexed) of all the values
+of the nodes in the tree.
+
+Example:
+    Input: root = [3, 1, 4, null, 2], k = 1
+    Output: 1
+"""
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def kth_smallest(root: TreeNode, k: int) -> int:
+    # Time Complexity: O(n)
+    stack = []
+    current = root
+    while current is not None or stack is not None:
+        while current is not None:
+            stack.append(current)
+            current = current.left
+        stack.pop()
+        k -= 1
+        if k == 0:
+            return current.val
+        current = current.right
+    return -1

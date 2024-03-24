62b2703174533355b16d884559b3c3e723088bdf
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-23 17:27:56 -0700

    Add Neetcode Linked List: Remove Nth Node From End of List

---
+++ NeetCode/Linked List/19_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,31 @@
+"""
+19. Remove Nth Node From End of List
+
+Given the head of a linked list,
+remove the nth node from the end of the list and return its head.
+
+Example:
+    Input: head = [1, 2, 3, 4, 5], n = 2
+    Output: [1, 2, 3, 5]
+"""
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+def remove_nth_from_end(head: ListNode, n: int) -> ListNode | None:
+    # Time Complexity: O(n)
+    sentinel = ListNode(0, head)
+    left = sentinel
+    right = head
+    while n > 0 and right is not None:
+        right = right.next
+        n -= 1
+    while right is not None:
+        left = left.next
+        right = right.next
+    left.next = left.next.next
+    return sentinel.next

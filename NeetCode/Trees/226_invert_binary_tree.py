1c234e89d3943b28085e69513350690c6314cce9
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-23 20:19:21 -0700

    Add Neetcode Trees: Invert Binary Tree

---
+++ NeetCode/Trees/226_invert_binary_tree.py
@@ -0,0 +1,28 @@
+"""
+226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example:
+    Input: root = [4, 2, 7, 1, 3, 6, 9]
+    Output: [4, 7, 2, 9, 6, 3, 1]
+"""
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def invert_tree(root: TreeNode) -> TreeNode | None:
+    if root is None:
+        return None
+
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+    invert_tree(root.left)
+    invert_tree(root.right)
+    return root

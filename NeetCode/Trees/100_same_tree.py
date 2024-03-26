a80aade43566ca6828ac997aa69674c5249ac3c3
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-03-26 09:11:42 -0700

    Change logical AND to OR

--- NeetCode/Trees/100_same_tree.py
+++ NeetCode/Trees/100_same_tree.py
@@ -24,6 +24,6 @@ def is_same_tree(p: TreeNode, q: TreeNode) -> bool:
     # Time Complexity: O(p + q)
     if p is None and q is None:
         return True
-    if (p is None or q is None) and (p.val != q.val):
+    if (p is None or q is None) or (p.val != q.val):
         return False
     return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
